3addb6a447c86d4444b1b9806f94733ae203544d
sylafont <118721662+sylafont@users.noreply.github.com>
2022-11-25 09:12:45 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -32,8 +32,54 @@ char GenererCaractereAleatoire()
     return listCaracteres[cIndex];
 }
 
-fffff
+void Tour(int n, char[,]doubleCache, char[,]doubleVisible ) // n représente le nombre de uplet
+{
+    int i =1;
+    int[] coordonnee = new int[2];
+    int[] ancienCoordonnee = new int[2];
+    ChoixCarte(coordonnee);
+    while(i <= n && perdu == false)
+    {
+        doubleVisible[coordonnee[0],coordonnee[1]] = doubleCache[coordonnee[0],coordonnee[1]];
+        MiseAJourTableau(doubleVisible);
+        ancienCoordonnee[0]= coordonnee[0];
+        ancienCoordonnee[1] = coordonnee[1];
+        ChoixCarte(coordonnee);
+        doubleVisible[coordonnee[0],coordonnee[1]] = doubleCache[coordonnee[0],coordonnee[1]];
+        MiseAJourTableau(doubleVisible);
 
+        if(doubleCache[coordonnee[0],coordonnee[1]] =! doubleCache[ancienCoordonnee[0],ancienCoordonnee[1]])
+        {
+            Echec(coordonnee, ancienCoordonnee);  // configurer fonction echec
+        }
+
+        i++;
+    }
+
+    if (perdu == false)
+    {
+
+    }
+
+}
+
+void ChoixCarte(int[] tab)
+{
+    int s=0;
+    do{
+        if(s!=0)
+        {
+            Console.WriteLine("Cette carte est déjà retourné");
+        }
+        Console.WriteLine("Veuillez indiquer le n° de colomne de la carte que vous souhaitez retourner : "); //Penser à mettre une sureté pour que l'uttilisateur ne puisse rentrer qu'un nombre
+        tab[0] =  Convert.ToInt32(Console.ReadLine()!);
+        Console.WriteLine("Veuillez indiquer le n° de ligne de la carte que vous souhaitez retourner : ");
+        tab[1] =  Convert.ToInt32(Console.ReadLine()!);
+        s++;
+    }
+    while(doubleCache[tab[0],tab[1]] == doubleVisible[tab[0], tab[1]]);
+    
+}
 
 
 
