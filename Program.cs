61ef7aca354134230c260c85589a7a428851f6ec
{Sybille Lafont} <{sylafont@ensc.fr}>
2022-11-25 11:11:20 +0100

    J avais oublié de sauvegarder

--- Program.cs
+++ Program.cs
@@ -1,24 +1,28 @@
 ﻿void Tour(int n, char[,]doubleCache, char[,]doubleVisible ) // n représente le nombre de uplet
 {
-    int i =1;
-    int[] coordonnee = new int[2];
-    int[] ancienCoordonnee = new int[2];
-    ChoixCarte(coordonnee, doubleCache, doubleVisible);
+    bool perdu = false;
+    int i =1; // initialisation du compteur de tour
+    int s = 0; // initialisation de la variable compteur de coordonnée
+    int[] coordonnee = new int[n*2];
+    //int[] ancienCoordonnee = new int[2];
+    //ChoixCarte(1, coordonnee, doubleCache, doubleVisible);
     while(i <= n && perdu == false)
     {
         doubleVisible[coordonnee[0],coordonnee[1]] = doubleCache[coordonnee[0],coordonnee[1]];
         MiseAJourTableau(doubleVisible);
-        ancienCoordonnee[0]= coordonnee[0];
-        ancienCoordonnee[1] = coordonnee[1];
-        ChoixCarte(coordonnee, doubleCache, doubleVisible);
-        doubleVisible[coordonnee[0],coordonnee[1]] = doubleCache[coordonnee[0],coordonnee[1]];
+        //ancienCoordonnee[0]= coordonnee[0];
+        //ancienCoordonnee[1] = coordonnee[1];
+        
+        ChoixCarte(i,coordonnee, doubleCache, doubleVisible);
+        doubleVisible[coordonnee[s],coordonnee[s+1]] = doubleCache[coordonnee[s],coordonnee[s+1]];
         MiseAJourTableau(doubleVisible);
 
-        if(doubleCache[coordonnee[0],coordonnee[1]] =! doubleCache[ancienCoordonnee[0],ancienCoordonnee[1]])
+        if(i!= 1 && doubleCache[coordonnee[s],coordonnee[s+1]] != doubleCache[coordonnee[s-2],coordonnee[s-1]])
         {
             Echec(i,coordonnee, doubleVisible);  // configurer fonction echec
+            perdu = true;
         }
-
+        s= s+2;
         i++;
     }
     //commencer nouveau tour dans le main
