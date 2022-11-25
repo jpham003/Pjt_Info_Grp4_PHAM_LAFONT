27bbabe3bc97dc61bcb50eac065f7f995652d4b5
{Sybille Lafont} <{sylafont@ensc.fr}>
2022-11-25 09:57:24 +0100

    Enlever fonction ChoixCarte

--- Program.cs
+++ Program.cs
@@ -63,25 +63,6 @@ void Tour(int n, char[,]doubleCache, char[,]doubleVisible ) // n représente le 
 
 }
 
-void ChoixCarte(int[] tab)
-{
-    int s=0;
-    do{
-        if(s!=0)
-        {
-            Console.WriteLine("Cette carte est déjà retourné");
-        }
-        Console.WriteLine("Veuillez indiquer le n° de colomne de la carte que vous souhaitez retourner : "); //Penser à mettre une sureté pour que l'uttilisateur ne puisse rentrer qu'un nombre
-        tab[0] =  Convert.ToInt32(Console.ReadLine()!);
-        Console.WriteLine("Veuillez indiquer le n° de ligne de la carte que vous souhaitez retourner : ");
-        tab[1] =  Convert.ToInt32(Console.ReadLine()!);
-        s++;
-    }
-    while(doubleCache[tab[0],tab[1]] == doubleVisible[tab[0], tab[1]]);
-    
-}
-
-
 
 
 
