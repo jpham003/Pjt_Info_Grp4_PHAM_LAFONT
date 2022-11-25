80d7f0b4ebb024c6c4d18219d92be7e27a5d3bfc
{Sybille Lafont} <{sylafont@ensc.fr}>
2022-11-25 11:15:05 +0100

    modification de la facon de gérer les coordonnées

--- Program.cs
+++ Program.cs
@@ -1,18 +1,18 @@
-﻿void ChoixCarte(int[] tab, char[,]doubleCache, char[,]doubleVisible)
+﻿void ChoixCarte(int s, int[] tab, char[,]doubleCache, char[,]doubleVisible)
 {
-    int s=0;
+    int i=0;
     do{
-        if(s!=0)
+        if(i!=0)
         {
             Console.WriteLine("Cette carte est déjà retourné");
         }
         Console.WriteLine("Veuillez indiquer le n° de colomne de la carte que vous souhaitez retourner : "); //Penser à mettre une sureté pour que l'uttilisateur ne puisse rentrer qu'un nombre
-        tab[0] =  Convert.ToInt32(Console.ReadLine()!);
+        tab[s] =  Convert.ToInt32(Console.ReadLine()!);
         Console.WriteLine("Veuillez indiquer le n° de ligne de la carte que vous souhaitez retourner : ");
-        tab[1] =  Convert.ToInt32(Console.ReadLine()!);
-        s++;
+        tab[s+1] =  Convert.ToInt32(Console.ReadLine()!);
+        i++;
     }
-    while(doubleCache[tab[0],tab[1]] == doubleVisible[tab[0], tab[1]]);
+    while(doubleCache[tab[s],tab[s+1]] == doubleVisible[tab[s], tab[s+1]]);
     
 }
 
